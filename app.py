62624e7ccae5cddb57d5d6075fb33ad92b35e5ec
Aryan Pahari <76030680+Aryan-coder-student@users.noreply.github.com>
2025-01-15 10:12:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ nltk.download('punkt', download_dir=nltk_data_path)
 nltk.download('wordnet', download_dir=nltk_data_path)
 nltk.download('omw-1.4', download_dir=nltk_data_path)
 nltk.download('punkt_tab', download_dir=nltk_data_path)
-=
 nltk.data.path.append(nltk_data_path)
 __import__('pysqlite3')
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
